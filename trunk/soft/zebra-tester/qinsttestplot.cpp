de2cf32d378ad8751d3c183dc0ef697d40b0f0ee
unknown <roc@.(none)>
2011-03-13 09:43:11 +0800

    co

---
+++ trunk/soft/zebra-tester/qinsttestplot.cpp
@@ -0,0 +1,12 @@
+#include "qinsttestplot.h"
+
+QInstTestPlot::QInstTestPlot(QWidget *parent)
+	: QwtPlot(parent)
+{
+	ui.setupUi(this);
+}
+
+QInstTestPlot::~QInstTestPlot()
+{
+
+}
---
+++ trunk/soft/zebra-tester/qinsttestplot.h
@@ -0,0 +1,19 @@
+#ifndef QINSTTESTPLOT_H
+#define QINSTTESTPLOT_H
+
+#include <QwtPlot>
+#include "ui_qinsttestplot.h"
+
+class QInstTestPlot : public QwtPlot
+{
+	Q_OBJECT
+
+public:
+	QInstTestPlot(QWidget *parent = 0);
+	~QInstTestPlot();
+
+private:
+	Ui::QInstTestPlotClass ui;
+};
+
+#endif // QINSTTESTPLOT_H
