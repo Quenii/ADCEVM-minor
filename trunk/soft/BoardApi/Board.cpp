a035890c2aeb70be14451063f714dd2b99460496
unknown <roc@.(none)>
2011-05-23 20:52:18 +0800

    c

--- trunk/soft/BoardApi/Board.cpp
+++ trunk/soft/BoardApi/Board.cpp
@@ -1,15 +1,236 @@
 #include "..\include\gkhy\BoardApi\Board.h"
+#include "CyAPI.h"
 
-Board::Board(void)
+#include <windows.h>
+#include <qmath.h>
+#include <vector>
+#include <Dbt.h>
+#include <QDebug>
+#include <QString>
+#include <QEvent>
+#include <QList>
+#include <QTimer>
+#include <QSettings>
+
+#pragma comment(lib, "CyAPI.lib")
+
+using namespace std;
+
+class DummyWidget : public QWidget
 {
+	Q_OBJECT
+
+public:
+	DummyWidget(QWidget* parent = 0, Qt::WindowFlags f = 0 );
+
+protected:
+	bool winEvent(MSG * message, long * result);
+
+signals:
+	void devChanged();
+
+private:
+	bool bPnP_Arrival;
+	bool bPnP_DevNodeChange;
+	bool bPnP_Removal;
+};
+
+
+DummyWidget::DummyWidget(QWidget* parent /*= 0*/, Qt::WindowFlags f /*= 0*/ ) : QWidget(parent, f) 
+{
+	bPnP_Arrival = false;
+	bPnP_DevNodeChange = false;
+	bPnP_Removal = false;
+	QTimer::singleShot(0, this, SIGNAL(devChanged()));	
 }
 
-Board::~Board(void)
-{
+bool DummyWidget::winEvent(MSG * message, long * result)
+{	
+	if (message->message == WM_DEVICECHANGE) {
+		// Tracks DBT_DEVICEARRIVAL followed by DBT_DEVNODES_CHANGED
+		if (message->wParam == DBT_DEVICEARRIVAL) {
+			bPnP_Arrival = true;
+			bPnP_DevNodeChange = false;
+		}
+		// Tracks DBT_DEVNODES_CHANGED followed by DBT_DEVICEREMOVECOMPLETE
+		if (message->wParam == DBT_DEVNODES_CHANGED) {
+			bPnP_DevNodeChange = true;
+			bPnP_Removal = false;
+		}
+		if (message->wParam == DBT_DEVICEREMOVECOMPLETE) {
+			bPnP_Removal = true;
+			PDEV_BROADCAST_HDR bcastHdr = (PDEV_BROADCAST_HDR) message->lParam;
+			if (bcastHdr->dbch_devicetype == DBT_DEVTYP_HANDLE) {
+				PDEV_BROADCAST_HANDLE pDev = (PDEV_BROADCAST_HANDLE) message->lParam;
+				/*if (pDev->dbch_handle == USBDevice->DeviceHandle())
+				USBDevice->Close();*/
+			}
+		}
+		// If DBT_DEVNODES_CHANGED followed by DBT_DEVICEREMOVECOMPLETE
+		if (bPnP_Removal && bPnP_DevNodeChange) {
+			//Sleep(10);
+			//DisplayDevices();
+			bPnP_Removal = false;
+			bPnP_DevNodeChange = false;
+			//gkhy::MfcMinus::Win32App::sleep(10);
+			emit devChanged();
+		}
+		// If DBT_DEVICEARRIVAL followed by DBT_DEVNODES_CHANGED
+		if (bPnP_DevNodeChange && bPnP_Arrival) {
+			//DisplayDevices();
+			bPnP_Arrival = false;
+			bPnP_DevNodeChange = false;
+			emit devChanged();
+		}
+	}
+	return false;
 }
 
-Board* Board::instance()
+Board* Board::_inst = 0;
+Board::Board(QObject* parent /* = 0 */) 
+: QObject(parent)
+, pi(3.141592653589793f)
+, m_timerId(0)
 {
-	static Board _inst;
-	return &_inst;
+	widget = new DummyWidget();
+	bool okay = connect(widget, SIGNAL(devChanged()), this, SLOT(devChanged()));
+	Q_ASSERT(okay);
+	usbDev = new CCyUSBDevice((HANDLE)(widget->winId()));
+}
+
+Board::~Board()
+{
+	if (usbDev)
+	{
+		delete usbDev;
+		usbDev = 0;
+	}
+
+	if (widget)
+	{
+		delete widget;
+		widget = 0;
+	}
+}
+
+void Board::devChanged()
+{
+	QList<AdcBoardInfo> devList;
+	if (!(usbDev->DeviceCount()))
+	{
+		devList.clear();
+	}
+	for (int i = 0; i < usbDev->DeviceCount(); ++i)
+	{
+		if (usbDev->Open(i))
+		{
+			AdcBoardInfo info;
+			info.usbAddr = i;
+			info.devName = QString(usbDev->DeviceName);
+			info.infName = QString(usbDev->FriendlyName);
+			devList.push_back(info);
+		}
+	}
+
+	emit devListChanged(devList);
+}
+
+bool Board::open(int usbAddr)
+{
+	return usbDev->Open((UCHAR)usbAddr);
+}
+
+bool Board::read(unsigned short addr, unsigned short *buf, unsigned int len)
+{	
+	if (! usbDev->BulkOutEndPt)
+		return false;
+
+	long b2Read = (len * sizeof(unsigned short) + 511) / 512 * 512;
+	long w2Read = b2Read / 2;
+	if (bulkIOBuff.size() < w2Read) bulkIOBuff.resize(w2Read);
+
+	if (!writeIOCmd(addr, true, w2Read))
+	{
+		return false;
+	}
+
+	long bRead = b2Read;
+	if (!usbDev->BulkInEndPt->XferData((unsigned char*)&bulkIOBuff[0], bRead))
+		return false;
+
+	if (b2Read != bRead)
+	{
+		return false;
+	}
+
+	memcpy(buf, &bulkIOBuff[0], len * sizeof(unsigned short));
+
+	return true;
+}
+
+bool Board::writeIOCmd(unsigned short addr, bool dirRead, unsigned short data)
+{	
+	if (! usbDev->BulkOutEndPt)
+		return false;
+
+	if (bulkIOBuff.size() < 4) bulkIOBuff.resize(4);
+
+	bulkIOBuff[0] = 0xbc95;
+	bulkIOBuff[1] = addr;
+	bulkIOBuff[2] = dirRead ?  0xFF00 : 0x00FF;
+	bulkIOBuff[3] = data;
+	long llen = 4 * sizeof(unsigned short);
+	if (!usbDev->BulkOutEndPt->XferData((UCHAR*)&bulkIOBuff[0], llen))
+		return false;
+
+	return true;
+}
+
+bool Board::readReg(unsigned short addr, unsigned short &val)
+{
+	return read(addr, &val, 1);
+}
+
+bool Board::writeReg(unsigned short addr, unsigned short val)
+{
+	if (!writeIOCmd(addr, false, val))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool Board::readReg24b(unsigned short addr,unsigned short& val)
+{
+	unsigned short w1 = (addr & 0xFF) << 8;
+	unsigned short w2 = (addr & 0xFF00) >> 8 | 0x80;
+	unsigned short r;
+	if (!writeReg(0x1002, w1))
+		return false;
+
+	gkhy::MfcMinus::Win32App::sleep(100);
+
+	if (!writeReg(0x1003, w2))
+		return false;
+
+	gkhy::MfcMinus::Win32App::sleep(300);
+
+	unsigned short temp[1024];
+
+	memset(temp, 0, sizeof(temp));
+	if ( !read(0x1002, temp, 1024)  )
+	{
+		return false;
+	}
+	val = temp[0];
+
+	//if ( !read(0x1002, &buff[0], 1024)  )
+	//{
+	//	return false;
+	//}
+
+	//	val = buff[0];
+
+	return 	true;
 }
--- trunk/soft/include/gkhy/BoardApi/Board.h
+++ trunk/soft/include/gkhy/BoardApi/Board.h
@@ -2,13 +2,85 @@
 
 #include "boardapi_global.h"
 
-class BOARDAPI_EXPORT Board
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QList>
+#include <QWidget>
+#include <vector>
+#include <QSettings>
+
+class CCyUSBDevice;
+class DummyWidget;
+class QObject;
+class QEvent;
+
+struct BoardInfo 
 {
-public:
-	Board* instance();
+	int usbAddr;
+	QString devName;
+	QString infName;
+};
+
+class Board : public QObject
+{
+	Q_OBJECT
 
 private:
-	Board(void);
+	static Board* _inst;
+
 public:
-	virtual ~Board(void);
+	static Board* instance() 
+	{
+		if (!_inst)
+		{
+			_inst = new Board(0);
+		}
+
+		return _inst;
+	}
+
+public:	
+	virtual ~Board();
+
+private:
+	Board(QObject* parent = 0);
+	Q_DISABLE_COPY(Board);
+
+public:
+	bool readReg(unsigned short addr,unsigned short& val);
+	bool writeReg(unsigned short addr,unsigned short val);
+
+	bool readReg24b(unsigned short addr,unsigned short& val);
+	bool writeReg24b(unsigned short addr,unsigned short val);
+
+protected:	
+	// len - number of unsigned-short's
+	bool read(unsigned short addr, unsigned short* buf, unsigned int len);
+	// len - number of unsigned-short's
+	//bool write(unsigned int addr, const unsigned short* buf, int len);
+
+
+private:
+	bool writeIOCmd(unsigned short addr, bool dirRead, unsigned short data);
+	unsigned short CalcReg(float v);
+
+signals:
+	void devListChanged(const QList<BoardInfo>& lst);
+
+public slots:
+		bool open(int usbAddr);
+private slots:
+    	void devChanged();
+
+private:
+	CCyUSBDevice* usbDev;
+	DummyWidget* widget;
+	std::vector<unsigned short> bulkIOBuff;
+	std::vector<unsigned short> buff;
+
+	const static int buffer_cnt = 32 * 1024;
+	const float pi;
 };
+
