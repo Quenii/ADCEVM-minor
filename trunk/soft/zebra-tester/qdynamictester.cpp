0c38e47e5ea5e401e4b7d07a6f902bfab75eb6b3
Quenii <Quenii@gmail.com>
2011-06-06 21:33:01 +0800

    C

---
+++ trunk/soft/zebra-tester/qdynamictester.cpp
@@ -0,0 +1,114 @@
+#include "qdynamictester.h"
+#include "types.h"
+#include "dacanalyzersettings.h"
+#include "DacBoard.h"
+#include "QMultiMeter.h"
+
+#include <cmath>
+#include <QMessageBox>
+#include <QThread>
+#include <QPointer>
+#include <QVector>
+#include <Qdir>
+#include <QTime>
+#include <QDate>
+#include <QCoreApplication>
+#include <QTextStream>
+
+class QThreadL : public QThread
+{
+public:
+	static void msleep (unsigned long msecs)
+	{
+		QThread::msleep(msecs);
+	}
+};
+
+static void msleep (unsigned long msecs)
+{
+	QThreadL::msleep(msecs);
+}
+
+QDynamicTester::QDynamicTester(QObject *parent)
+	: QObject(parent)
+	, pi(3.141592653589793f)
+	, m_bStarted(false)
+{
+
+}
+
+QDynamicTester::~QDynamicTester()
+{
+
+}
+
+QDynamicTester* QDynamicTester::instance()
+{
+	static QDynamicTester _instance;
+	return &_instance;
+}
+
+bool QDynamicTester::start()
+{
+	if (m_bStarted) 
+		return false;
+
+	DacAnalyzerSettings s;
+	
+	m_dynamicTestSettings = s.dynamicTestSettings();
+	m_dacTypeSettings = s.dacTypeSettings();
+
+	DacBoard* board = DacBoard::instance();
+
+	board->close();
+	if (!board->open())
+	{
+		QMessageBox::critical(0, "", QString::fromLocal8Bit("´ò¿ª°å¿¨Ê§°Ü¡£"));
+		return false;
+	}
+	std::vector<unsigned short> buff;
+	const int buffer_cnt = 20000;
+
+	board->writeReg(0x1008, 0x0001); //bit 15: static/dynamic; bit 0: reset dut;
+	msleep(100);
+
+	board->writeReg(0x1008, 0x0000); //bit 15: static/dynamic; bit 0: reset dut;
+
+	board->writeReg(0x1004, 0);
+	if (buff.size() < buffer_cnt)
+		buff.resize(buffer_cnt);
+
+	float max = (1 << (m_dacTypeSettings.bitCount - 1));
+
+	float fs = m_dynamicTestSettings.clockFreq;
+	float fc = m_dynamicTestSettings.signalFreq;
+
+
+	for (int i=0; i<buffer_cnt; ++i)
+	{
+		buff[i] = ((short)((sin(2*pi*i*fc/fs)+1)*max));
+	}
+
+	bool okay = board->write(0x1005, &buff[0], 20*1000*4);
+	board->writeReg(0x1006, 0);
+	board->writeReg(0x1007, 1);
+
+	m_bStarted = true;
+	emit started();
+
+	
+	return true;
+}
+
+ 
+void QDynamicTester::stop()
+{
+	if (!m_bStarted) return ;
+
+	m_bStarted = false;
+
+	//Board::instance()->close();
+
+	emit stopped();
+}
+
---
+++ trunk/soft/zebra-tester/qdynamictester.h
@@ -0,0 +1,49 @@
+#ifndef QDYNAMICTESTER_H
+#define QDYNAMICTESTER_H
+
+#include "types.h"
+
+#include <QObject>
+#include <QWeakPointer>
+#include <QSharedPointer>
+#include <QFile>
+
+
+class QDynamicTester : public QObject
+{
+	Q_OBJECT
+
+public:
+	static QDynamicTester* instance();
+
+private:
+	QDynamicTester(QObject *parent = 0);
+	Q_DISABLE_COPY(QDynamicTester)
+
+public:
+	~QDynamicTester();
+
+signals:
+	void started();
+	void stopped();
+
+public:
+	bool start();
+	void stop();
+	bool isRunning() { return m_bStarted; }
+	
+protected:
+	
+private:	
+	bool m_bStarted;
+	int m_timerId;
+	const float pi;
+	
+	DacTypeSettings m_dacTypeSettings;
+	DynamicTestSettings m_dynamicTestSettings;
+	unsigned int m_currentVal;	
+
+	QFile m_file;
+};
+
+#endif // QDYNAMICTESTER_H
